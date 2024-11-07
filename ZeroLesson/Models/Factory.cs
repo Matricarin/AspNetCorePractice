e59a2cad03d294c012d2098eb9f30e98105635e0
Roman Urmancheev <matricarin@gmail.com>
2024-11-07 12:53:52 +0500

    refactored

--- ZeroLesson/Models/Factory.cs
+++ ZeroLesson/Models/Factory.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace ZeroLesson.Models
 {
     public class Factory
     {
         public int Id { get; set;}
-        public string Name{ get; set;}
-        public string Description{ get; set;}
+        public string? Name{ get; set;}
+        public string? Description{ get; set;}
     }
 }
--- ZeroLesson/Models/Tank.cs
+++ ZeroLesson/Models/Tank.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace ZeroLesson.Models
 {
     public class Tank
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
         public int Volume{ get; set; }
         public int MaxVolume{ get; set; }
         public int UnitId { get; set; }
